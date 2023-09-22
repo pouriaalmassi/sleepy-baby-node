979ba856425b97b11c17725f7f34eaa6faa1b395
Pouria Almassi <pouria@almassi.com>
2023-09-22 11:55:53 +1000

    Return a user's sleeps.

--- src/routers/sleep.js
+++ src/routers/sleep.js
@@ -21,8 +21,6 @@ router.get("/sleeps", auth, async (req, res) => {
   const match = {}
   const sort = {}
 
-  // TODO left off the `completed` matching
-
   if (req.query.sortBy) {
     const parts = req.query.sortBy.split(":")
     sort[parts[0]] = parts[1] === "desc" ? -1 : 1
@@ -31,13 +29,15 @@ router.get("/sleeps", auth, async (req, res) => {
   try {
     await req.user.populate({
       path: "sleeps",
-      match: match,
+      match,
       options: {
         limit: parseInt(req.query.limit),
         skip: parseInt(req.query.skip),
         sort,
       },
     }).execPopulate()
+
+    res.send({ "response": req.user.sleeps })
   } catch (e) {
     console.log(`Error: ${e}`)
     res.status(500).send()
