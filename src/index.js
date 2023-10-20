2624f0d2049fdd61d0bdd9cc46f724228e94a621
Pouria Almassi <pouria@almassi.com>
2023-10-20 14:16:22 +1100

    Debug messages.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ const app = require("./app")
 const port = process.env.PORT
 
 app.listen(port, () => {
-  console.log(`Server is listening http://127.0.0.1:${port}`)
+  console.log(`Server started.`)
+  console.log(`MONGODB_URL: ${MONGODB_URL}`)
 })
